760a9e86f746abcbd96b1db8f21e754da25751cb
vdominika <vdominika@yahoo.com>
2017-09-15 13:47:57 +0200

    adding palindrome searching function

---
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -0,0 +1,45 @@
+//import java.util.Scanner;
+//
+public class PalindromeSearcher {
+//    public static void main (String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Please give me a word:");
+//        String userInput1 = scanner.nextLine();
+//
+//        String word = userInput1;
+//
+        System.out.println("Finding all palindromes in a given string : ");
+        subPal("abcacbbbca");
+
+        public static void subPal(String str) {
+            String s1 = "";
+            int N = str.length(), count = 0;
+            Set<String> palindromeArray = new HashSet<String>();
+            System.out.println("Given string : " + str);
+            System.out.println("******** Ignoring single character as substring palindrome");
+            for (int i = 2; i <= N; i++) {
+                for (int j = 0; j <= N; j++) {
+                    int k = i + j - 1;
+                    if (k >= N)
+                        continue;
+                    s1 = str.substring(j, i + j);
+                    if (s1.equals(new StringBuilder(s1).reverse().toString())) {
+                        palindromeArray.add(s1);
+                    }
+                }
+
+            }
+            System.out.println(palindromeArray);
+            for (String s : palindromeArray)
+                System.out.println(s + " - is a palindrome string.");
+            System.out.println("The no.of substring that are palindrome : "
+                    + palindromeArray.size());
+        }
+
+
+    }
+
+
+}}
