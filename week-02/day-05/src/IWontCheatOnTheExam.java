4b71a043eadf1b78776b26aed37af07914d450a9
vdominika <vdominika@yahoo.com>
2017-09-18 01:27:10 +0200

    IWontCheatInTheExam

---
+++ week-02/day-05/src/IWontCheatOnTheExam.java
@@ -0,0 +1,8 @@
+public class IWontCheatOnTheExam {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print("I wont't cheat in the exam!");
+        }
+    }
+}
