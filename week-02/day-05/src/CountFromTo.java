dc90178b3e2412655d87774dd9d2de1623593958
vdominika <vdominika@yahoo.com>
2017-09-18 01:54:44 +0200

    CountFromTo

---
+++ week-02/day-05/src/CountFromTo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountFromTo {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int a = scanner.nextInt();
+
+        System.out.println("Please enter another number");
+        int b = scanner.nextInt();
+
+        if (a >= b) {
+            System.out.println("The first number should be bigger");
+
+        else {
+            for( b, b > a; b--);
+            }
+        }
+    }
+}
