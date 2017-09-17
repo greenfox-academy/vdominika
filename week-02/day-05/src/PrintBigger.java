638b2b0ad288d5d0f8cee742d1fc0c9e59e33ab2
vdominika <vdominika@yahoo.com>
2017-09-18 01:06:26 +0200

    PrintBigger

---
+++ week-02/day-05/src/PrintBigger.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintBigger {
+    public static void main (String[] arg);{
+
+    Scanner myScanner = new Scanner(System.in){
+
+        System.out.println("Please give me a number:");
+        int a = myScanner.nextInt();
+
+        System.out.println("Please give me another number:");
+        int b = myScanner.nextInt();
+
+        if (a > b){
+        System.out.println(a)}
+
+        else{
+                System.out.println(b); }
+    }
+    }
+    }
