9fef34783a619d208e719ee1f38a38190518e75e
vdominika <vdominika@yahoo.com>
2017-09-19 17:47:36 +0200

    CopyFile

--- week-03/day-2/src/CopyFile.java
+++ week-03/day-2/src/CopyFile.java
@@ -1,21 +1,25 @@
-/*// Write a function that copies a file to an other
+// Write a function that copies a file to an other
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
-public class WriteFile {
+import static com.sun.deploy.cache.Cache.copyFile;
+
+public class CopyFile {
     public static void main(String[] args) {
-
-        copyFile
-
         try {
-            Path filePath = Paths.get("my-second-file.txt");
-            Files.write(filePath, content);
+            Path filePath = Paths.get("kiskece.txt");
+            List<String> lines = Files.readAllLines(filePath);
+            Path filePath2 = Paths.get("felszallott.txt");
+            Files.write(filePath2, lines);
         } catch (Exception e) {
             System.out.println("Uh-oh, could not write the file!");
         }
     }
-}*/
+}
