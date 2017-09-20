abc999ca6963ebed6bb8d5d7d400db3e9d20066c
vdominika <vdominika@yahoo.com>
2017-09-20 13:08:39 +0200

    Position Squares

---
+++ week-03/day-3/src/PositionSquare.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        graphics.setColor (Color.RED);
+        DrawRect (180, 60, graphics);
+        DrawRect (50, 60, graphics);
+        DrawRect (210, 150, graphics);
+    }
+
+    public static void DrawRect(int x, int y, Graphics graphics) {
+
+        graphics.drawRect (x + 50, y - 50, 50, 50);
+
+    }
+
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
