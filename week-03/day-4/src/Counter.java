def3c693d77b99e42fb697c8a8371f6206551538
vdominika <vdominika@yahoo.com>
2017-09-21 21:08:33 +0200

    Adjusting Counter

--- week-03/day-4/src/Counter.java
+++ week-03/day-4/src/Counter.java
@@ -1,14 +1,14 @@
-// Write a recursive function that takes one parameter: n and counts down from n.
-//public class Counter {
-//    public static void main (String[] args);{
-//
-//        public static void counter ( int n){
-//        if (n == 1) {
-//            return 0;
-//        }
-//        System.out.println ( );
-//        return counter (n - 1);
-//        }
-//    }
-//}
+public class Counter {
+    public static void main(String[] args) {
+        countingBack(5);
+    }
 
+    private static void countingBack(int number) {
+        if (number == 0) {
+            System.out.println(number);
+        } else {
+            System.out.println(number);
+            countingBack(number - 1);
+        }
+    }
+}
