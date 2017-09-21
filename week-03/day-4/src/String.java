34f1e51cfafc19d3b1262917f74fb115e460a674
vdominika <vdominika@yahoo.com>
2017-09-21 15:29:38 +0200

    fibonacci

---
+++ week-03/day-4/src/String.java
@@ -0,0 +1,10 @@
+/*// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+public class String {
+    public static void main(String[] args) {
+    int String: "Ebben a szovegben sok az x, de meg tobb az xxxx";
+    }
+    public static int string1(String){
+        if ()
+    }
+}*/
