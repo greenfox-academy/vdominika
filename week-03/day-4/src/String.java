2deaad2380139695fb7fe3377ad123298fef6d00
vdominika <vdominika@yahoo.com>
2017-09-21 21:28:41 +0200

    adjusting String

--- week-03/day-4/src/String.java
+++ week-03/day-4/src/String.java
@@ -1,18 +1,18 @@
-/*// Given a string, compute recursively (no loops) a new string where all the
+// Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
 public class String {
-    public static void main(String[] args) {
-    String s = "Ebben a szovegben sok az x, de meg tobb az xxxx";
-    string1 (s);
-    }
-    public static String string1(String s){
-        if (s.length() == 0){
-            return "";
-        } else if (s.endWith ("x")) {
-            return "y";
-        }else
-            return string1(s.substring(s.length()-1) + )
 
+    public static void main(String[] args) {
+        System.out.println(string1("abcdxxxbb"));
+    }
+
+    public static String string1(String input) {
+
+        if (input.length() == 0) {
+            return "";
+        }
+        else {
+            return string1(input.substring(0, input.length() - 1)) + input.substring(input.length() - 1).replace("x", "y");
+        }
     }
 }
-*/
