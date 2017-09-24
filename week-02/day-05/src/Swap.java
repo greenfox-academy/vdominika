d0ba78f65575117d5967168ded2af26f25f32c80
vdominika <vdominika@yahoo.com>
2017-09-25 01:11:38 +0200

    Swap element

--- week-02/day-05/src/Swap.java
+++ week-02/day-05/src/Swap.java
@@ -1,2 +1,16 @@
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+
 public class Swap {
+    public static void main(String[] args) {
+        String[] abc = {"first", "second", "third"};
+        String swap = abc[0];
+        abc[0] = abc[2];
+        abc[2] = swap;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(abc[i]);
+        }
+    }
 }
