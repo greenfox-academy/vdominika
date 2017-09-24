8d894144017670e9a30c54112315e57c5e774271
vdominika <vdominika@yahoo.com>
2017-09-25 01:29:21 +0200

    Add elements

---
+++ week-02/day-05/src/SumElements.java
@@ -0,0 +1,13 @@
+
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai`
+
+public class SumElements {
+    public static void main(String[] args) {
+        int[] ai = {3, 4, 5, 6, 7};
+        System.out.println (ai[0] + ai[1] +ai[2] + ai[3] + ai[4]);
+    }
+}
+
+
