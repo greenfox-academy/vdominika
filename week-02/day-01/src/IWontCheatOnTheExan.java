c562c34430b9bcbf3bdd88d188d3828c0c813356
vdominika <vdominika@yahoo.com>
2017-09-30 23:55:00 +0200

    I Wont Cheat In the Exam! task finished

---
+++ week-02/day-01/src/IWontCheatOnTheExan.java
@@ -0,0 +1,8 @@
+public class IWontCheatOnTheExan {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println ("I Wont Cheat In the Exam!" );
+        }
+    }
+}
