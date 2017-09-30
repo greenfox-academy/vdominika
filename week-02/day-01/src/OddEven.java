1a115af1598e37cb56de7e5d887f1ad38659980f
vdominika <vdominika@yahoo.com>
2017-09-30 19:43:59 +0200

    redoing OddEven

---
+++ week-02/day-01/src/OddEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OddEven {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println ("Please give me a number" );
+        int userNumber = myScanner.nextInt ( );
+         if (userNumber % 2 ==0){
+             System.out.println ("It is even!" );
+         }else{
+             System.out.println ("It is odd!" );
+         }
+    }
+}
