e0938aa4abbd004a05a850b3e20b1906513bb8a4
vdominika <vdominika@yahoo.com>
2017-10-01 00:11:48 +0200

    MultiplicationTable done

---
+++ week-02/day-01/src/MultiplicationTable.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner (System.in);
+        System.out.println ("Please enter a number");
+        int a = myScanner.nextInt ();
+
+        System.out.println ("1 * "+ a +" = "+ 1 * a );
+        System.out.println ("2 * "+ a +" = "+ 2 * a );
+        System.out.println ("3 * "+ a +" = "+ 3 * a );
+        System.out.println ("4 * "+ a +" = "+ 4 * a );
+        System.out.println ("5 * "+ a +" = "+ 5 * a );
+        System.out.println ("6 * "+ a +" = "+ 6 * a );
+        System.out.println ("7 * "+ a +" = "+ 7 * a );
+        System.out.println ("8 * "+ a +" = "+ 8 * a );
+        System.out.println ("9 * "+ a +" = "+ 9 * a );
+        System.out.println ("10 * "+ a +" = "+ 10 * a );
+
+    }
+}
