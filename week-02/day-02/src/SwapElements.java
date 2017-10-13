4c471fefefc378cc7152a3163f4c6b81af1201a7
vdominika <vdominika@yahoo.com>
2017-10-13 20:41:31 +0200

    correcting SwapElements

---
+++ week-02/day-02/src/SwapElements.java
@@ -0,0 +1,20 @@
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+public class SwapElements {
+
+    public static void main(String[] args) {
+
+        String[] abc = {"first", "second", "third"};
+
+        String c = abc[0];
+        abc[0] = abc[2];
+        abc[2] = c;
+
+        for (int i = 0; i < abc.length; i++) {
+
+            System.out.println (abc[i] );
+        }
+    }
+
+}
