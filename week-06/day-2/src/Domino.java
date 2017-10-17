d32c2874b2d6b4789ae6738076cedacbca803606
vdominika <vdominika@yahoo.com>
2017-10-17 14:29:25 +0200

    finishing Domino excercise

--- week-06/day-2/src/Domino.java
+++ week-06/day-2/src/Domino.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Domino implements Comparable<Domino>{
+public class Domino implements Comparable<Domino> {
     private final int[] values;
 
     public Domino(int valueA, int valueB) {
@@ -21,16 +21,16 @@ public class Domino implements Comparable<Domino>{
 
     public static void main(String[] args) {
 
-        List<Domino> dominoes = new ArrayList<> ();
-        dominoes.add(new Domino(5, 2));
-        dominoes.add(new Domino(4, 6));
-        dominoes.add(new Domino(1, 5));
-        dominoes.add(new Domino(6, 7));
-        dominoes.add(new Domino(2, 4));
-        dominoes.add(new Domino(7, 1));
+        List<Domino> dominoes = new ArrayList<> ( );
+        dominoes.add (new Domino (5, 2));
+        dominoes.add (new Domino (4, 6));
+        dominoes.add (new Domino (1, 5));
+        dominoes.add (new Domino (6, 7));
+        dominoes.add (new Domino (2, 4));
+        dominoes.add (new Domino (7, 1));
 
 // Java version
-        Collections.sort(dominoes);
+        Collections.sort (dominoes);
         for (Domino d : dominoes) {
             System.out.println (d);
         }
@@ -38,14 +38,12 @@ public class Domino implements Comparable<Domino>{
 
     public int compareTo(Domino compareValue) {
 
-        return compareValue.getValues ()[0] - this.getValues ()[0];
+        if (this.getValues ( )[0] == compareValue.getValues ( )[0] && this.getValues ( )[1] < compareValue.getValues ( )[1]) {
+            return -1;
+        } else if (this.getValues ( )[0] > compareValue.getValues ( )[0]) {
+            return -1;
+        } else {
+            return 1;
+        }
     }
-
 }
-//    public int compareTo(Domino compareValue) {
-//
-//        if (this.getValues ()[0] - compareValue.getValues ()[0] > 0)
-//            return 1;
-//        else if(this.getValues ()[0] - compareValue.getValues ()[0] <= 0)
-//            return -1;
-//    }
