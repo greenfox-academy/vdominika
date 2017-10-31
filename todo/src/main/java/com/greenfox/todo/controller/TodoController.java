68cbe0b4242cd8668a539a37dc6d6fa2cbf4ab7b
vdominika <vdominika@yahoo.com>
2017-10-31 11:06:30 +0100

    Created template

--- todo/src/main/java/com/greenfox/todo/controller/TodoController.java
+++ todo/src/main/java/com/greenfox/todo/controller/TodoController.java
@@ -2,12 +2,13 @@ package com.greenfox.todo.controller;
 
 import com.greenfox.todo.repository.TodoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping ("/todo")
 public class TodoController {
 
@@ -15,8 +16,7 @@ public class TodoController {
     TodoRepo todoRepo;
 
     @RequestMapping({"/","/list"})
-    @ResponseBody
     public String list(Model model){
-        return "This is my first todo";
+        return "todo";
     }
 }
--- todo/src/main/java/com/greenfox/todo/model/Todo.java
+++ todo/src/main/java/com/greenfox/todo/model/Todo.java
@@ -1,7 +1,13 @@
 
 package com.greenfox.todo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
     int id;
     String title;
     boolean isUrgent;
