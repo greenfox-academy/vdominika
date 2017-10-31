37b6062452f7851528ab8b637c6337dabb3feac0
vdominika <vdominika@yahoo.com>
2017-10-31 12:57:23 +0100

    Finished 1st task

--- todo/src/main/java/com/greenfox/todo/TodoApplication.java
+++ todo/src/main/java/com/greenfox/todo/TodoApplication.java
@@ -1,12 +1,24 @@
 package com.greenfox.todo;
 
+import com.greenfox.todo.model.Todo;
+import com.greenfox.todo.repository.TodoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner{
+
+	@Autowired
+	TodoRepo todoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		todoRepo.save(new Todo("I have to learn Object Relational Mapping"));
 	}
 }
--- todo/src/main/java/com/greenfox/todo/model/Todo.java
+++ todo/src/main/java/com/greenfox/todo/model/Todo.java
@@ -12,4 +12,44 @@ public class Todo {
     String title;
     boolean isUrgent;
     boolean isDone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isUrgent() {
+        return isUrgent;
+    }
+
+    public void setUrgent(boolean urgent) {
+        isUrgent = urgent;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public Todo() {
+
+    }
+
+    public Todo(String s) {
+        this.title = s;
+    }
 }
