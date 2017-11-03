0669005e2c5bb08756c373f09e0c8316bd9af52e
vdominika <vdominika@yahoo.com>
2017-11-03 12:11:25 +0100

    Creating application properties

--- week-08/day-5/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
+++ week-08/day-5/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
@@ -1,9 +1,11 @@
 package com.greenfoxacademy.reddit.controller;
 
+import com.greenfoxacademy.reddit.model.Post;
 import com.greenfoxacademy.reddit.repository.RedditRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -12,5 +14,32 @@ public class RedditController {
     @Autowired
     RedditRepo redditRepo;
 
+    @RequestMapping({"/","/posts"})
+    public String list(Model model){
+        model.addAttribute("posts", redditRepo.findAll());
+        return "posts";
+    }
 
+    @GetMapping(value = "/add")
+    public String loadCreate() {
+        return "add";
+    }
+
+    @PostMapping(value = "/add")
+    public String createTodo(@RequestParam String content) {
+        redditRepo.save(new Post(content));
+        return "redirect:/posts/";
+    }
+
+    @GetMapping(value = "/{id}/edit")
+    public String loadTodo(@PathVariable long id, Model model) {
+        model.addAttribute("posts", redditRepo.findOne(id));
+        return "posts";
+    }
+
+    @PostMapping(value = "/{id}/edit")
+    public String editTodo(@PathVariable long id, @ModelAttribute Post post) {
+        redditRepo.save(post);
+        return "redirect:/posts/";
+    }
 }
