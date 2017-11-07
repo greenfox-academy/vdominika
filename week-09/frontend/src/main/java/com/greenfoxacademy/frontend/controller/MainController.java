73151da70ab1abcd81157518bbb8924d28936dea
vdominika <vdominika@yahoo.com>
2017-11-07 11:17:35 +0100

    creating task

---
+++ week-09/frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping({"/"})
+    public String list (){
+        return index.html;
+    }
+    
+}
