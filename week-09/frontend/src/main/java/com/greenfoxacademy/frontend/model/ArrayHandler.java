49f6fbf1f85a9eaeb01b691517914c6b7e819ee9
vdominika <vdominika@yahoo.com>
2017-11-07 17:42:44 +0100

    started array handling

---
+++ week-09/frontend/src/main/java/com/greenfoxacademy/frontend/model/ArrayHandler.java
@@ -0,0 +1,38 @@
+package com.greenfoxacademy.frontend.model;
+
+import java.util.ArrayList;
+
+public class ArrayHandler {
+
+    String what;
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what;
+    }
+
+    public ArrayList<Integer> getNumber() {
+        return number;
+    }
+
+    public void setNumber(ArrayList<Integer> number) {
+        this.number = number;
+    }
+
+    public ArrayHandler() {
+    }
+
+    public ArrayHandler(String what, ArrayList<Integer> number) {
+
+        this.what = what;
+        this.number = number;
+    }
+
+    ArrayList<Integer> number;
+
+    ActionOfArrayHandler()
+        return.type ResponseEntity
+}
